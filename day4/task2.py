daa042f48c0be938da501a3da7dcfb020662b0b2
a.krasnov <icrasnow@icloud.com>
2020-12-05 12:53:46 +0300

    hotfix

--- day4/task2.py
+++ day4/task2.py
@@ -1,6 +1,7 @@
 def check_sum(spisok_chisel, summa):
     for i in range(len(spisok_chisel)):
-        if spisok_chisel[i] + spisok_chisel[i+1] == summa:
+        if spisok_chisel[i] + spisok_chisel[i + 1] == summa:
             return True
+
 
 check_sum([1, 3, 2, 12, 11], 5)
