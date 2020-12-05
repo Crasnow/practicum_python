cc842942c90c40e78fefc232e4602c02f76523d2
a.krasnov <icrasnow@icloud.com>
2020-12-05 18:23:34 +0300

    return old version

--- day2/task1.py
+++ day2/task1.py
@@ -3,9 +3,9 @@ number = int(input('Введите максимальную длину: '))
 
 print(len(text), len(''.join(text.split())))
 
-if len(text) % 2 == 0:
+if len(text) // 2 == 0:
     print('количество символов в тексте четное')
-elif len(text) % 2 != 0:
+elif len(text) // 2 != 0:
     print('количество символов в тексте нечетное')
 else:
     print('не удалось определить четность символов в тексте. Количество символов: ' + len(text))
