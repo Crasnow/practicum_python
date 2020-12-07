6993804d019e447119d7738e9e89dc37fa4f8c5f
a.krasnov <icrasnow@icloud.com>
2020-12-07 19:33:16 +0300

    hotfix

--- day3/task2.py
+++ day3/task2.py
@@ -1,21 +1,24 @@
 import re
 import json
 
-new_text = input()
+text = input()
 count_words = int(input())
 symbols = '[,|!|?|.]'
 
-clear_text_symbols = (re.sub(symbols, '', new_text).lower()).split(' ')
-clear_text_double = set()
+text_as_set_wo_symbols = (re.sub(symbols, '', text).lower()).split(' ')
+text_as_set_wo_doubles = set()
 dict_words = {}
 
-for i in clear_text_symbols:
-    clear_text_double.add(i)
+for i in text_as_set_wo_symbols:
+    text_as_set_wo_doubles.add(i)
 
-for i in clear_text_double:
-    count = clear_text_symbols.count(i)
+for i in text_as_set_wo_doubles:
+    count = text_as_set_wo_symbols.count(i)
 
     if count >= count_words:
         dict_words[i] = count
 
 parasite_words_json = json.dumps(dict_words, ensure_ascii=False)
+
+print(count)
+
--- day4/task1.py
+++ day4/task1.py
@@ -1,8 +1,8 @@
 def check_sum(spisok_chisel, summa):
-    for i in spisok_chisel:
-        for k in spisok_chisel:
-            if i + k == summa:
+    for i in range(len(spisok_chisel) - 1):
+        for k in range(i + 1, len(spisok_chisel)):
+            if spisok_chisel[i] + spisok_chisel[k] == summa:
                 return True
 
 
-check_sum([1, 3, 2, 12, 11], 5)
+print(check_sum([1, 3, 2, 12, 11], 5))
