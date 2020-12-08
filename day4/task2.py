c960123cf9007269f8c3cc39db55839011cb8503
a.krasnov <icrasnow@icloud.com>
2020-12-08 20:17:06 +0300

    hotfix

--- day4/task2.py
+++ day4/task2.py
@@ -1,8 +1,10 @@
 def check_sum(spisok_chisel, summa):
-    for i in range(len(spisok_chisel) - 1):
-        for k in range(i + 1, len(spisok_chisel)):
-            if spisok_chisel[i] + spisok_chisel[k] == summa:
-                return True
+    my_dict_tmp = {}
+    for i in spisok_chisel:
+        if my_dict_tmp.setdefault(i) is None:
+            my_dict_tmp[summa - i] = True
+        else:
+            return my_dict_tmp[i]
 
 
 check_sum([1, 3, 2, 12, 11], 5)
