870cd53e08bbc093f9b51402cb29d79d5195d30f
a.krasnov <icrasnow@icloud.com>
2020-12-15 09:29:10 +0300

    hotfix

--- day10/task2.py
+++ day10/task2.py
@@ -1,5 +1,9 @@
-def number_translation(number, rank):
+def number_translation(number, rank=1):
     result = ''
+
+    if rank <= 0:
+        print("Введите существую систему счисления.")
+        return
 
     while number >= rank:
         temp_number = number // rank
